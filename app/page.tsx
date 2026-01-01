36c2ecc78f12b325a56c8d584dd867dcc2d16567
Sinan <msinannoufal@gmail.com>
2026-01-01 08:37:51 +0400

    blogs issue fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ export default function Home() {
   return (
     <main className="mx-auto w-11/12 flex flex-col gap-6 mb-20">
       <h1 className="font-cormorant-garamond font-light text-6xl text-neutral-900 text-center">Welcome to my portfolio</h1>
-      <p className="text-center text-neutral-700">Click "Blogs" in the navbar to view posts.</p>
     </main>
   );
 }
